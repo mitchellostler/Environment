3555b4b50b18b553ceee5ff06b87101d35aaa47a
Mitchell Ostler <mlostler@uwaterloo.ca>
2022-05-02 09:42:26 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Mac or linux specific behaviour
 if [[ $OSTYPE == 'darwin'* ]]; then
   echo "Mac Specific Setup..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew install tmux
+  #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  #brew install tmux
 else
   echo "Linux Specific Setup..."
   sudo apt-get install tmux	
